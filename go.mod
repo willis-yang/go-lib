a3d3251a9b2a177461973dc8b88e502d9c66e796
willis <willis@qq.com>
2023-11-14 20:19:43 +0800

    build(*) init project

---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/WillisGitHub/go-lib
+
+go 1.19
+
+require (
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/zeromicro/go-zero v1.6.0
+	gorm.io/driver/mysql v1.5.2
+	gorm.io/gorm v1.25.5
+)
+
+require (
+	github.com/fatih/color v1.15.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/spaolacci/murmur3 v1.1.0 // indirect
+	go.opentelemetry.io/otel v1.19.0 // indirect
+	go.opentelemetry.io/otel/trace v1.19.0 // indirect
+	go.uber.org/automaxprocs v1.5.3 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+)
---
+++ stores/db/custom-gorm.go
@@ -0,0 +1,90 @@
+package db
+
+import (
+	"fmt"
+	"github.com/zeromicro/go-zero/core/logx"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
+	"time"
+)
+
+// 自定义数据库连接池
+// 支持读写分离模式
+// 查询缓存
+type GormConfig struct {
+	//dns 连接参数：https://github.com/go-sql-driver/mysql#dsn-data-source-name
+	DSN            string // dsh连接 [username[:password]@][protocol[(address)]]/dbname[?param1=value1&...&paramN=valueN]
+	TablePrefix    string // 表前缀
+	DatabaseType   string // 连接驱动类型
+	MaxIdLe        int    //空闲最大连接数
+	MaxConnect     int    //最大连接数
+	ConnectMaxLife int    //可复用连接最长时间
+}
+
+const DatabaseTypeMysql = "mysql"
+const DatabaseTypePostgreSQL = "PostgreSQL"
+const DatabaseTypeTiDB = "TiDB"
+
+// 初始化gorm 连接池
+func newGorm(gormConfig GormConfig, logConfig logx.LogConf) *gorm.DB {
+
+	var (
+		filePath  string
+		newLogger logger.Interface
+	)
+	if logConfig.Path == "" { //无配置文件类型，直接输出日志
+		newLogger = logger.New(
+			log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer（日志输出的目标，前缀和日志包含的内容——译者注）
+			logger.Config{
+				SlowThreshold:             time.Second, // 慢 SQL 阈值
+				LogLevel:                  logger.Info, // 日志级别
+				IgnoreRecordNotFoundError: false,       // 忽略ErrRecordNotFound（记录未找到）错误
+				Colorful:                  false,       // 禁用彩色打印
+			},
+		)
+	} else {
+		filePath = fmt.Sprintf("%v/sql-%v.log", logConfig.Path, time.Now().Format("2006-01-02"))
+		file, _ := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE|os.O_APPEND, os.ModePerm)
+		newLogger = logger.New(
+			log.New(file, "\r\n", log.LstdFlags), // 文件
+			//log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer（日志输出的目标，前缀和日志包含的内容——译者注）
+			logger.Config{
+				SlowThreshold:             time.Second, // 慢 SQL 阈值
+				LogLevel:                  logger.Info, // 日志级别
+				IgnoreRecordNotFoundError: false,       // 忽略ErrRecordNotFound（记录未找到）错误
+				Colorful:                  false,       // 禁用彩色打印
+			},
+		)
+	}
+
+	db, err := gorm.Open(mysql.New(mysql.Config{
+		DSN:                       "gorm:gorm@tcp(127.0.0.1:3306)/gorm?charset=utf8&parseTime=True&loc=Local", // DSN data source name
+		DefaultStringSize:         256,                                                                        // string 类型字段的默认长度
+		DisableDatetimePrecision:  true,                                                                       // 禁用 datetime 精度，MySQL 5.6 之前的数据库不支持
+		DontSupportRenameIndex:    true,                                                                       // 重命名索引时采用删除并新建的方式，MySQL 5.7 之前的数据库和 MariaDB 不支持重命名索引
+		DontSupportRenameColumn:   true,                                                                       // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
+		SkipInitializeWithVersion: false,                                                                      // 根据当前 MySQL 版本自动配置
+	}), &gorm.Config{
+		Logger: newLogger,
+	})
+
+	if err != nil {
+		//连接异常直接抛异常
+		panic(err)
+	}
+
+	sqlDB, err := db.DB()
+	// 设置空闲连接池中的最大连接数
+	sqlDB.SetMaxIdleConns(gormConfig.MaxIdLe)
+
+	//设置与数据库的最大打开连接数
+	sqlDB.SetMaxOpenConns(gormConfig.MaxConnect)
+
+	// 设置可重复使用连接的最长时间
+	sqlDB.SetConnMaxLifetime(time.Duration(gormConfig.ConnectMaxLife))
+
+	return db
+}
