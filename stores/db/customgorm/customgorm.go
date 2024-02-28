321f53c8ba04ff5e870c815429e299fc050f5bd8
willis <willis@qq.com>
2024-02-28 10:04:42 +0800

    feat(customgorm.go) add custom mysql connect package

---
+++ stores/db/customgorm/customgorm.go
@@ -0,0 +1,84 @@
+package customgorm
+
+import (
+	"fmt"
+	"github.com/zeromicro/go-zero/core/logx"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
+	"time"
+)
+
+type GormConfig struct {
+	DSN            string // dsh [username[:password]@][protocol[(address)]]/dbname[?param1=value1&...&paramN=valueN]
+	TablePrefix    string
+	DatabaseType   string
+	MaxIdLe        int
+	MaxConnect     int
+	ConnectMaxLife int
+}
+
+const DatabaseTypeMysql = "mysql"
+const DatabaseTypePostgreSQL = "PostgreSQL"
+const DatabaseTypeTiDB = "TiDB"
+
+// 初始化gorm 连接池
+func NewGorm(gormConfig GormConfig, logConfig logx.LogConf) *gorm.DB {
+
+	var (
+		filePath  string
+		newLogger logger.Interface
+	)
+	if logConfig.Path == "" { //无配置文件类型，直接输出日志
+		newLogger = logger.New(
+			log.New(os.Stdout, "\r\n", log.LstdFlags),
+			logger.Config{
+				SlowThreshold:             time.Second,
+				LogLevel:                  logger.Info,
+				IgnoreRecordNotFoundError: false,
+				Colorful:                  false,
+			},
+		)
+	} else {
+		filePath = fmt.Sprintf("%v/sql-%v.log", logConfig.Path, time.Now().Format("2006-01-02"))
+		file, _ := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE|os.O_APPEND, os.ModePerm)
+		newLogger = logger.New(
+			log.New(file, "\r\n", log.LstdFlags),
+			logger.Config{
+				SlowThreshold:             time.Second,
+				LogLevel:                  logger.Info,
+				IgnoreRecordNotFoundError: false,
+				Colorful:                  false,
+			},
+		)
+	}
+
+	var gormDialector gorm.Dialector
+
+	if gormConfig.DatabaseType == DatabaseTypeMysql {
+		gormDialector = mysql.New(mysql.Config{
+			DSN:                       gormConfig.DSN,
+			DefaultStringSize:         256,
+			DisableDatetimePrecision:  true,
+			DontSupportRenameIndex:    true,
+			DontSupportRenameColumn:   true,
+			SkipInitializeWithVersion: false,
+		})
+	}
+	db, err := gorm.Open(gormDialector, &gorm.Config{
+		Logger: newLogger,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	sqlDB, err := db.DB()
+	sqlDB.SetMaxIdleConns(gormConfig.MaxIdLe)
+	sqlDB.SetMaxOpenConns(gormConfig.MaxConnect)
+	sqlDB.SetConnMaxLifetime(time.Duration(gormConfig.ConnectMaxLife))
+
+	return db
+}
---
+++ stores/db/customgorm/customgorm_test.go
@@ -0,0 +1,38 @@
+package customgorm
+
+import (
+	"github.com/zeromicro/go-zero/core/logx"
+	"testing"
+)
+
+func TestNewGorm(t *testing.T) {
+	gorm := NewGorm(GormConfig{
+		DSN:            "root:xxxxx@tcp(xxxxx:3306)/release_manage?charset=utf8mb4&parseTime=true&loc=Local&timeout=3600s",
+		TablePrefix:    "",
+		DatabaseType:   DatabaseTypeMysql,
+		MaxIdLe:        0,
+		MaxConnect:     0,
+		ConnectMaxLife: 0,
+	}, logx.LogConf{
+		ServiceName:         "",
+		Mode:                "",
+		Encoding:            "",
+		TimeFormat:          "",
+		Path:                "",
+		Level:               "",
+		MaxContentLength:    0,
+		Compress:            false,
+		Stat:                false,
+		KeepDays:            0,
+		StackCooldownMillis: 0,
+		MaxBackups:          0,
+		MaxSize:             0,
+		Rotation:            "",
+	})
+	var list []string
+	err := gorm.Select("id,name").Where("id = ?", 10).Table("test").Find(&list).Error
+	if err != nil {
+		panic(err)
+	}
+	logx.Info(list)
+}
