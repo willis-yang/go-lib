b9a9908a7c1123cd52b25b42aebb0c442f9f6ca2
willis <willis@qq.com>
2026-01-07 20:52:33 +0800

    feat(*) add ssh connect

---
+++ utils/ssh_test.go
@@ -0,0 +1,48 @@
+package utils
+
+import (
+	"testing"
+)
+
+func TestNewSSHManager(t *testing.T) {
+	type args struct {
+		host    string
+		port    string
+		user    string
+		keyPath string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *SSHManager
+		wantErr bool
+	}{
+		{
+			name: "测试连接 SSH",
+			args: args{
+				host:    "192.168.10.5",
+				port:    "22",
+				user:    "root",
+				keyPath: "id_rsa",
+			},
+			want:    nil,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			connect, err := NewSSHManager(tt.args.host, tt.args.port, tt.args.user, tt.args.keyPath)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewSSHManager() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			defer connect.Close()
+			output, err := connect.ExecuteCommand("ps -ef |grep ios")
+			if err != nil {
+				t.Errorf("ExecuteCommand() error = %v", err)
+				return
+			}
+			t.Logf("ExecuteCommand() output = %v", output)
+		})
+	}
+}
